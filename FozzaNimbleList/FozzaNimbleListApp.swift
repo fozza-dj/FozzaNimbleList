c2dfbcb9ac6fdd9bdf466738bf158f61bcc37ee1
dingjie.0611 <dingjie.0611@bytedance.com>
2025-07-24 16:37:05 +0800

    Initial Commit

---
+++ FozzaNimbleList/FozzaNimbleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FozzaNimbleListApp.swift
+//  FozzaNimbleList
+//
+//  Created by ByteDance on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FozzaNimbleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
