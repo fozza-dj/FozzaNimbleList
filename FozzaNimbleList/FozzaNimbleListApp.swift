95162c998292dcf67b3791b50c2bf6dabbfa80e3
dingjie.0611 <dingjie.0611@bytedance.com>
2025-07-24 16:41:30 +0800

    feature: init project

--- FozzaNimbleList/FozzaNimbleListApp.swift
+++ FozzaNimbleList/FozzaNimbleListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FozzaNimbleListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
---
+++ FozzaNimbleList/MainTabView.swift
@@ -0,0 +1,53 @@
+//
+//  MainTabView.swift
+//  FozzaNimbleList
+//
+//  Created by ByteDance on 7/24/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            // 首页标签
+//            NavigationStack {
+//                HomeView()
+//            }
+            Text("朱/紫")
+            .tabItem {
+                Image(systemName: "book.fill")
+                Text("全国图鉴")
+            }
+
+            // 其他标签页
+            Text("朱/紫")
+                .tabItem {
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("朱/紫")
+                }
+
+            Text("我的队伍")
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("我的队伍")
+                }
+
+            Text("专题")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("专题")
+                }
+
+            Text("工具")
+                .tabItem {
+                    Image(systemName: "wrench.fill")
+                    Text("工具")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
